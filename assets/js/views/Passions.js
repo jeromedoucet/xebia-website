39e5389f6db8548b3639a387e3d71d407acef541
sophietk <sophie.tk@gmail.com>
2012-07-22 23:21:42 +0200

    Using jParallax and SlidesJS libs

--- assets/js/views/Passions.js
+++ assets/js/views/Passions.js
@@ -1,12 +1,19 @@
-define(['text!/assets/tmpl/passions.html'], function (source) {
+define(['text!/assets/tmpl/passions.html',
+    'lib/jquery.parallax.min',
+    'lib/slides.min.jquery'],
+    function (source) {
     return Backbone.View.extend({
     	el: '#passions > .content',
     	template: Handlebars.compile(source),
     	initialize: function() {
     		$(this.template()).appendTo(this.$el).hide().fadeIn().slideDown();
+            this.$('.parallax-layer').parallax();
+            this.$('#slides').slides({
+                prev: 'prev',
+                next: 'next'
+            });
     	},
     	render: function() {
-    		
     	}
     });
 });
