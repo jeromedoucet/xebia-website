3c7ff526c98ffa1ffed78c137c08ecf33af55d8e
Mathieu Breton <mbreton@xebia.fr>
2012-10-09 01:06:03 +0200

    Test préloading

--- assets/js/Xebia.js
+++ assets/js/Xebia.js
@@ -38,4 +38,28 @@ require(['router', 'jquery', 'underscore', 'backbone', 'handlebars', 'less', 'jq
 
         new Router();
         Backbone.history.start();
-    });
+
+        var $loaderContainer = $('#percent-container');
+        var $percent = $('#percent');
+        var $imgs = $('img');
+        var nbElToPreload = $imgs.length, nbElPreloaded = 0;
+        $imgs.hide();
+        $loaderContainer.show();
+        $imgs.each(function (idx, item){
+            var img = new Image();
+            img.onload = function()
+            {
+                if (++nbElPreloaded == nbElToPreload){
+                    $imgs.show();
+                    $loaderContainer.fadeOut('slow');
+                }
+                $loaderContainer.append('<div style="text-align:left;">> '+this.src+'</div>');
+                var percentVal = nbElPreloaded / nbElToPreload *100;
+                $percent.html(parseInt(percentVal) +"%");
+                $percent.width(percentVal+"%");
+
+            }
+            img.src = $(item).attr('src');
+        });
+    }
+);
