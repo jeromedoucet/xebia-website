318ef19e962ee9665137842c92849f0e6e172c28
Benoit Lemoine <blemoine@xebia.fr>
2014-09-12 20:55:54 +0200

    Tectrends 4

--- data/techtrends/techtrends-data.js
+++ data/techtrends/techtrends-data.js
@@ -106,5 +106,38 @@ window.TECHTRENDS = [
             title: 'L’AGILITÉ EN MUTATION',
             content: "De l’eau a coulé sous les ponts depuis les premières tentatives d’équipes Scrum en France au début des années 2000. À cette époque, les équipes de développement cherchaient à transformer les chefs de projet technique en Scrum Master, les MOA en Product Owner. Aujourd’hui, la culture de l’agilité est ancrée dans les moeurs, les PO et facilitateurs sont recrutés pour ces métiers. Le résultat est flagrant dans nos organisations : le management visuel, l’esprit d’équipe, la démarche d’amélioration continue sont maintenant des acquis pour de nombreuses équipes. Les méthodes agiles sont arrivées à maturité et les équipes sont potentiellement efficaces. Potentiellement seulement, car le travail accompli jusqu’ici n’a optimisé qu’un maillon. Le développement n’est plus le goulot d’étranglement de la chaîne de valeur de la production logicielle. L’étape suivante consiste à considérer le processus de production logicielle dans son ensemble. Nous constatons une propagation de l’agilité aux interfaces des équipes de réalisation : vers le métier et vers les équipes de production."
         }
+    },
+    {
+        file: 'techtrends-xebia-craft.pdf',
+        date: '09/2014',
+        packshot:'packshot-craft.jpg',
+        summary: [
+            {
+                title: 'INITIER',
+                content: [
+                    "Alignez les visions de tous les participants du projet sur la notion de qualité logicielle",
+                    "Entretenez une équipe de craftsmen passionnés et impliqués"
+                ]
+            },
+            {
+                title: 'CONSTRUIRE',
+                content: [
+                    "Favorisez la communication au sein de l'équipe",
+                    "Laissez votre équipe choisir ses bons outils",
+                    "Encouragez un environnement propice à la réalisation d'un code propre"
+                ]
+            },
+            {
+                title: 'ENTRETENIR',
+                content: [
+                    "Aménagez des temps d'échange et de partage",
+                    "Miser sur la communauté pour ne pas rester en marge des innovations"
+                ]
+            }
+        ],
+        description: {
+            title: 'Les bâtisseurs de cathédrale du XXIème siècle',
+            content: "Aujourd’hui, de nombreuses entreprises ont adopté les processus agiles. Cependant, trop souvent les projets agiles déçoivent en se heurtant fréquemment à des problématiques telles que les pratiques de développement logiciel, la qualité intrinsèque des équipes et leur capacité à intégrer en continu de nouvelles méthodes de réalisation d’un logiciel de très haute qualité au juste coût. Cet amer constat a poussé les équipes de développement à agir. C’est ainsi qu’est né le mouvement Software Craftsmanship. Nous sommes convaincus que les bénéfices au sein de vos équipes sont réels : une base de code évolutive et à juste coût, une montée en compétences homogène de l’équipe et des engagements de développement en termes de délai et de qualité plus souvent respectés. Ce TechTrends vous permettra d’identifier l’attitude type des software craftsmen, de comprendre comment ils travaillent et de faire émerger ces comportements et ces pratiques au sein de vos équipes, pour en retirer tous les bénéfices."
+        }
     }
 ];
