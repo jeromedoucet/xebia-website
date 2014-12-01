d1caa03a05bf5f007037869b3237a979c3563b8d
blacroix <benjlacroix@gmail.com>
2014-12-01 14:09:03 +0100

    Update 2014-12-01T13:09:03.196Z

---
+++ data/services/mobile/clients-data.js
@@ -0,0 +1,9 @@
+window.CLIENTS = [
+    {name: 'Sephora'},
+    {name: 'Voyages-SNCF.com'},
+    {name: 'PMU'},
+    {name: 'Le Figaro'},
+    {name: 'Givaudan'},
+    {name: 'La Centrale.fr'},
+    {name: 'Orange'}
+];
